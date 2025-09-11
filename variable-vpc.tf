03e3f28322c84b4891a2cd2966bc434f47cd767e
YDLJ <100246525+lio72@users.noreply.github.com>
2025-09-11 14:43:09 +0200

    create vpc

---
+++ variable-vpc.tf
@@ -0,0 +1,26 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+    description = "EC2 Region for the VPC"
+    default = "us-east-1"
+}
+variable "availability_zone1" {
+    description = "Avaialbility Zones"
+    default = "us-east-1b"
+}
+
+variable "availability_zone2" {
+    description = "Avaialbility Zones"
+    default = "us-east-1c"
+}
+
+variable "availability_zone3" {
+    description = "Avaialbility Zones"
+    default = "us-east-1d"
+}
+
+variable "main_vpc_cidr" {
+    description = "CIDR of the VPC"
+    default = "10.0.0.0/16"
+}
