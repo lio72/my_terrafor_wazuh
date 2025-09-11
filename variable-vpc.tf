10ab5c9b4ea8055e7f2f9ce7d7e87210ed4e7e14
YDLJ <100246525+lio72@users.noreply.github.com>
2025-09-11 15:22:28 +0200

    ok

--- variable-vpc.tf
+++ variable-vpc.tf
@@ -1,5 +1,5 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
 
 variable "aws_region" {
     description = "EC2 Region for the VPC"
