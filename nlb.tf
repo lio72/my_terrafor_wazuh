878235e136361b15541b21d6712241c4c312f1f1
YDLJ <100246525+lio72@users.noreply.github.com>
2025-09-15 12:31:24 +0200

    ok

--- nlb.tf
+++ nlb.tf
@@ -1,47 +1,47 @@
-resource "aws_lb" "nlb_wazuh" {
-  name               = "wazuh-nlb"
-  internal           = false
-  load_balancer_type = "network"
-  security_groups    = [aws_security_group.wazuh-nlb.id]
-  subnets            = [, aws_subnet.public2.id]
+# resource "aws_lb" "nlb_wazuh" {
+#   name               = "wazuh-nlb"
+#   internal           = false
+#   load_balancer_type = "network"
+#   security_groups    = [aws_security_group.wazuh-nlb.id]
+#   subnets            = [, aws_subnet.public2.id]
 
-  enable_deletion_protection = false
-}
+#   enable_deletion_protection = false
+# }
 
-# Target Groups for ports 1514 and 1515
-resource "aws_lb_target_group" "tg_1514" {
-  name     = "tg-1514"
-  port     = 1514
-  protocol = "TCP"
-  vpc_id   = aws_vpc.main.id
-}
+# # Target Groups for ports 1514 and 1515
+# resource "aws_lb_target_group" "tg_1514" {
+#   name     = "tg-1514"
+#   port     = 1514
+#   protocol = "TCP"
+#   vpc_id   = aws_vpc.main.id
+# }
 
-resource "aws_lb_target_group" "tg_1515" {
-  name     = "tg-1515"
-  port     = 1515
-  protocol = "TCP"
-  vpc_id   = aws_vpc.main.id
-}
+# resource "aws_lb_target_group" "tg_1515" {
+#   name     = "tg-1515"
+#   port     = 1515
+#   protocol = "TCP"
+#   vpc_id   = aws_vpc.main.id
+# }
 
-# TLS Listeners with ACM Certificate
-resource "aws_lb_listener" "listener_1514" {
-  load_balancer_arn = aws_lb.nlb_wazuh.arn
-  port              = 1514
-  protocol          = "TCP"
+# # TLS Listeners with ACM Certificate
+# resource "aws_lb_listener" "listener_1514" {
+#   load_balancer_arn = aws_lb.nlb_wazuh.arn
+#   port              = 1514
+#   protocol          = "TCP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg_1514.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.tg_1514.arn
+#   }
+# }
 
-resource "aws_lb_listener" "listener_1515" {
-  load_balancer_arn = aws_lb.nlb_wazuh.arn
-  port              = 1515
-  protocol          = "TCP"
+# resource "aws_lb_listener" "listener_1515" {
+#   load_balancer_arn = aws_lb.nlb_wazuh.arn
+#   port              = 1515
+#   protocol          = "TCP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg_1515.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.tg_1515.arn
+#   }
+# }
