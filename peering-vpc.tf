cf369f3dcab76e4ac1dd43265e58cf6668dd942b
YDLJ <100246525+lio72@users.noreply.github.com>
2025-09-16 14:31:11 +0200

    ok

--- peering-vpc.tf
+++ peering-vpc.tf
@@ -26,12 +26,12 @@ resource "aws_vpc_peering_connection_accepter" "peer_accepter" {
 # Mise à jour des routes dans chaque VPC
 resource "aws_route" "route_to_peer_1" {
   route_table_id         = "rtb-05dc37ecf001e8080" # Remplace par la route table de VPC 1
-  destination_cidr_block = "10.0.0.0/16" # CIDR du VPC 2
+  destination_cidr_block = "172.31.0.0/16" # CIDR du VPC 2
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
 resource "aws_route" "route_to_peer_2" {
   route_table_id         = "rtb-0bf5c956acc4877e8" # Remplace par la route table de VPC 2
-  destination_cidr_block = "172.31.0.0/16" # CIDR du VPC 1
+  destination_cidr_block = "10.0.0.0/16" # CIDR du VPC 1
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
