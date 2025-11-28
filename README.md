631d10ea086f849b5c1099387889a2e08595c854
YDLJ <100246525+lio72@users.noreply.github.com>
2025-11-28 11:34:37 +0100

    deploy wazuh ec2

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ ssh-keygen -t rsa -b 4096 -C "yamkamlionel@gmail.com"
 ~/.ssh/ke 
 
 #### set vpc id in ec2.tf
+
+1*** deploy vpc
+2*** deploy ec2 nb: edit ec2 wazuh variables adding new vpc_id an new subnets_id
+
+3*** deploy NLb
+
--- ec2-wazuh.tf
+++ ec2-wazuh.tf
@@ -1,118 +1,118 @@
-# resource "aws_key_pair" "deployer" {
-#   key_name   = "deployer-key"
-#   public_key = file("~/.ssh/id_rsa.pub") # Path to your local public key
-# }
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub") # Path to your local public key
+}
 
-# ################### wazuh indexer ############################
+################### wazuh indexer ############################
 
-# resource "aws_instance" "bb_indexer_cluster" {
-#   for_each     = local.instance_wazuh_indexer 
-#   ami           = "ami-0b0012dad04fbe3d7" 
-#   key_name      = aws_key_pair.deployer.key_name
-#   instance_type = "t3.medium"
-#   monitoring    = true
-#   subnet_id     = each.value.subnet_id
-#   vpc_security_group_ids = [aws_security_group.wazuh_indexer_sg.id]
-#   root_block_device  {
-#     volume_size = 40
-#     volume_type = "gp3"
-#     delete_on_termination = true
-#     encrypted = true     
-#   }
+resource "aws_instance" "bb_indexer_cluster" {
+  for_each     = local.instance_wazuh_indexer 
+  ami           = "ami-0b0012dad04fbe3d7" 
+  key_name      = aws_key_pair.deployer.key_name
+  instance_type = "t3.medium"
+  monitoring    = true
+  subnet_id     = each.value.subnet_id
+  vpc_security_group_ids = [aws_security_group.wazuh_indexer_sg.id]
+  root_block_device  {
+    volume_size = 40
+    volume_type = "gp3"
+    delete_on_termination = true
+    encrypted = true     
+  }
 
-#   user_data = file("mount-ebs-index.sh")
-#   # Attach the EBS volume after creation
-#   depends_on = [aws_ebs_volume.aws_ebs_volume_wi] 
+  user_data = file("mount-ebs-index.sh")
+  # Attach the EBS volume after creation
+  depends_on = [aws_ebs_volume.aws_ebs_volume_wi] 
 
-#   tags = {
-#     Name = each.value.name
-#   }
-# }
+  tags = {
+    Name = each.value.name
+  }
+}
 
-# resource "aws_ebs_volume" "aws_ebs_volume_wi" {
-#   for_each     = local.instance_wazuh_indexer 
-#   availability_zone = each.value.az
-#   size              = 40
-#   type              = "gp3"
+resource "aws_ebs_volume" "aws_ebs_volume_wi" {
+  for_each     = local.instance_wazuh_indexer 
+  availability_zone = each.value.az
+  size              = 40
+  type              = "gp3"
 
-#   tags = {
-#     Name = "aws_ebs_wi_${each.value.az}"
-#   }
-# }
+  tags = {
+    Name = "aws_ebs_wi_${each.value.az}"
+  }
+}
 
-# resource "aws_volume_attachment" "ebs_wi_att" {
-#   count       = 3
-#   device_name = "/dev/xvdf"
+resource "aws_volume_attachment" "ebs_wi_att" {
+  count       = 3
+  device_name = "/dev/xvdf"
 
-#   volume_id   = local.volume_id_wi_set[count.index]
-#   instance_id = local.instance_id_wi_set[count.index]
-#   force_detach = true
-#   depends_on = [aws_ebs_volume.aws_ebs_volume_wi, aws_instance.bb_indexer_cluster]
-# }
+  volume_id   = local.volume_id_wi_set[count.index]
+  instance_id = local.instance_id_wi_set[count.index]
+  force_detach = true
+  depends_on = [aws_ebs_volume.aws_ebs_volume_wi, aws_instance.bb_indexer_cluster]
+}
 
-# ################### wazuh server ############################
+################### wazuh server ############################
 
-# resource "aws_instance" "bb_server_cluster" {
-#   for_each     = local.instance_wazuh_server 
-#   ami           = "ami-0b0012dad04fbe3d7"
-#   key_name      = aws_key_pair.deployer.key_name
-#   instance_type = "t3.medium"
-#   monitoring    = true
-#   subnet_id     = each.value.subnet_id
-#   vpc_security_group_ids = [aws_security_group.wazuh_server_sg.id]
-#   root_block_device  {
-#     volume_size = 30
-#     volume_type = "gp3"
-#     delete_on_termination = true
-#     encrypted = true     
-#   }
-#   user_data = file("mount-ebs-server.sh")
+resource "aws_instance" "bb_server_cluster" {
+  for_each     = local.instance_wazuh_server 
+  ami           = "ami-0b0012dad04fbe3d7"
+  key_name      = aws_key_pair.deployer.key_name
+  instance_type = "t3.medium"
+  monitoring    = true
+  subnet_id     = each.value.subnet_id
+  vpc_security_group_ids = [aws_security_group.wazuh_server_sg.id]
+  root_block_device  {
+    volume_size = 30
+    volume_type = "gp3"
+    delete_on_termination = true
+    encrypted = true     
+  }
+  user_data = file("mount-ebs-server.sh")
 
-#   tags = {
-#     Name = each.value.name
-#   }
-#   # Attach the EBS volume after creation
-#   depends_on = [aws_ebs_volume.aws_ebs_volume_ws]  
-# }
+  tags = {
+    Name = each.value.name
+  }
+  # Attach the EBS volume after creation
+  depends_on = [aws_ebs_volume.aws_ebs_volume_ws]  
+}
 
-# resource "aws_ebs_volume" "aws_ebs_volume_ws" {
-#   for_each     = local.instance_wazuh_server 
-#   availability_zone = each.value.az
-#   size              = 40
-#   type              = "gp3"
+resource "aws_ebs_volume" "aws_ebs_volume_ws" {
+  for_each     = local.instance_wazuh_server 
+  availability_zone = each.value.az
+  size              = 40
+  type              = "gp3"
 
-#   tags = {
-#     Name = "bb-ebs_wi_${each.value.az}"
-#   }
-# }
+  tags = {
+    Name = "bb-ebs_wi_${each.value.az}"
+  }
+}
 
-# resource "aws_volume_attachment" "ebs_ws_att" {
-#   count       = 2
-#   device_name = "/dev/xvdf"
-#   volume_id   = local.volume_id_ws_set[count.index]
-#   instance_id = local.instance_id_ws_set[count.index]
-#   force_detach = true
-#   depends_on = [aws_ebs_volume.aws_ebs_volume_ws, aws_instance.bb_server_cluster]
-# }
+resource "aws_volume_attachment" "ebs_ws_att" {
+  count       = 2
+  device_name = "/dev/xvdf"
+  volume_id   = local.volume_id_ws_set[count.index]
+  instance_id = local.instance_id_ws_set[count.index]
+  force_detach = true
+  depends_on = [aws_ebs_volume.aws_ebs_volume_ws, aws_instance.bb_server_cluster]
+}
 
 
-# #################wazuh dashboard #####################################
-# resource "aws_instance" "bb_dashboard" {
-#   ami           = "ami-0b0012dad04fbe3d7"
-#   key_name      = aws_key_pair.deployer.key_name
-#   instance_type = "t3.medium"
-#   monitoring    = true
-#   subnet_id     = "subnet-024267d66591859d1"
-#   associate_public_ip_address = "true"
-#   vpc_security_group_ids = [aws_security_group.wazuh_dashboard_sg.id]
-#   root_block_device  {
-#     volume_size = 30
-#     volume_type = "gp3"
-#     delete_on_termination = true
-#     encrypted = true     
-#   }
+#################wazuh dashboard #####################################
+resource "aws_instance" "bb_dashboard" {
+  ami           = "ami-0b0012dad04fbe3d7"
+  key_name      = aws_key_pair.deployer.key_name
+  instance_type = "t3.medium"
+  monitoring    = true
+  subnet_id     = "subnet-000db21987d261a0d"
+  associate_public_ip_address = "true"
+  vpc_security_group_ids = [aws_security_group.wazuh_dashboard_sg.id]
+  root_block_device  {
+    volume_size = 30
+    volume_type = "gp3"
+    delete_on_termination = true
+    encrypted = true     
+  }
 
-#   tags = {
-#     Name = "dashboard"
-#   }
-# }
+  tags = {
+    Name = "dashboard"
+  }
+}
