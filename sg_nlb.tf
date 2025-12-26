3c21b2ea006315af0e2cc3a4e42f850fce0c8eb6
YDLJ <100246525+lio72@users.noreply.github.com>
2025-12-26 16:48:31 +0100

    update

--- sg_nlb.tf
+++ sg_nlb.tf
@@ -1,45 +1,45 @@
-# resource "aws_security_group" "wazuh_nlb_sg" {
+resource "aws_security_group" "wazuh_nlb_sg" {
 
-#   name        = "bb_wazuh_nlb_sg"
-#   description = "Allow  on wazuh dashboar"
-#   vpc_id      = var.vpc_id
+  name        = "bb_wazuh_nlb_sg"
+  description = "Allow  on wazuh dashboar"
+  vpc_id      = var.vpc_id
 
-#   # Optional: Wazuh indexer RESTful API
+  # Optional: Wazuh indexer RESTful API
 
-#   ingress {
-#     description = "Agent connection service"
-#     from_port   = 1514
-#     to_port     = 1514
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    description = "Agent connection service"
+    from_port   = 1514
+    to_port     = 1514
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     description = "Agent enrollment service"
-#     from_port   = 1515
-#     to_port     = 1515
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    description = "Agent enrollment service"
+    from_port   = 1515
+    to_port     = 1515
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 514
-#     to_port     = 514
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 514
+    to_port     = 514
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name = "wazuh-nlb-sg"
-#   }
-# }
+  tags = {
+    Name = "wazuh-nlb-sg"
+  }
+}
 
 
