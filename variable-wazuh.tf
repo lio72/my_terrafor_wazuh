ee624d5f8bfc1499e4f9f5a0ab374176f3d64c62
YDLJ <100246525+lio72@users.noreply.github.com>
2026-01-07 10:00:50 +0100

    deploy infra

--- variable-wazuh.tf
+++ variable-wazuh.tf
@@ -1,85 +1,85 @@
-# # vpc subnet and az for wazuh
+# vpc subnet and az for wazuh
 
-# variable "vpc_id" {
-#   type = string
-#   default =  "vpc-08dc839d627f488d7"
-# }
+variable "vpc_id" {
+  type = string
+  default =  "vpc-0aac388e2057e7327"
+}
 
-# variable "subnet_ids" {
-#   type    = list(string)
-#   default = [
-#     "subnet-01dae7bbd198d7803",
-#     "subnet-090e80af0cbbeac95",
-#     "subnet-0ff304713313da7d0"
-#   ]
-# }
+variable "subnet_ids" {
+  type    = list(string)
+  default = [
+    "subnet-07493049be43d6321",
+    "subnet-0acbe4e8a2056d427",
+    "subnet-0f35371d1ed49fb36"
+  ]
+}
 
-# variable "subnet_az" {
-#   type    = list(string)
-#   default = [
-#     "us-east-1b",
-#     "us-east-1c",
-#     "us-east-1d"
-#   ]
-# }
+variable "subnet_az" {
+  type    = list(string)
+  default = [
+    "us-east-1b",
+    "us-east-1c",
+    "us-east-1d"
+  ]
+}
 
-# # variable for wazuh indexer cluster
+# variable for wazuh indexer cluster
 
-# variable "indexer_instance_names" {
-#   type    = list(string)
-#   default = [
-#     "bb-wazuh_indexer-1",
-#     "bb-wazuh_indexer-2",
-#     "bb-wazuh_indexer-3"
-#   ]
-# }
-# locals {
-#   instance_wazuh_indexer = {
-#     for idx in range(length(var.subnet_ids)) :
-#     var.indexer_instance_names[idx] => {
-#       subnet_id = var.subnet_ids[idx]
-#       name      = var.indexer_instance_names[idx]
-#       az        = var.subnet_az[idx]
-#     }
-#   }
-# }
+variable "indexer_instance_names" {
+  type    = list(string)
+  default = [
+    "bb-wazuh_indexer-1",
+    "bb-wazuh_indexer-2",
+    "bb-wazuh_indexer-3"
+  ]
+}
+locals {
+  instance_wazuh_indexer = {
+    for idx in range(length(var.subnet_ids)) :
+    var.indexer_instance_names[idx] => {
+      subnet_id = var.subnet_ids[idx]
+      name      = var.indexer_instance_names[idx]
+      az        = var.subnet_az[idx]
+    }
+  }
+}
 
 
-# # varible for wazuh server cluster
+# varible for wazuh server cluster
 
-# variable "server_instance_names" {
-#   type    = list(string)
-#   default = [
-#     "bb-wazuh_manager",
-#     "bb-wazuh_worker1",
-#     "bb-wazuh_worker2"
-#   ]
-# }
-# locals {
-#   instance_wazuh_server = {
-#     for idx in range(length(var.subnet_ids)) :
-#     var.server_instance_names[idx] => {
-#       subnet_id = var.subnet_ids[idx]
-#       name      = var.server_instance_names[idx]
-#       az        = var.subnet_az[idx]
-#     }
-#   }
-# }
+variable "server_instance_names" {
+  type    = list(string)
+  default = [
+    "bb-wazuh_manager",
+    "bb-wazuh_worker1",
+    "bb-wazuh_worker2"
+  ]
+}
+locals {
+  instance_wazuh_server = {
+    for idx in range(length(var.subnet_ids)) :
+    var.server_instance_names[idx] => {
+      subnet_id = var.subnet_ids[idx]
+      name      = var.server_instance_names[idx]
+      az        = var.subnet_az[idx]
+    }
+  }
+}
 
-# # variable for volume wazuh server attachment
-# locals {
-#   volume_id_ws_set = [for v in aws_ebs_volume.aws_ebs_volume_ws : v.id]
-# }
+# variable for volume wazuh server attachment
+locals {
+  volume_id_ws_set = [for v in aws_ebs_volume.aws_ebs_volume_ws : v.id]
+}
 
-# locals {
-#   instance_id_ws_set = [for v in aws_instance.bb_server_cluster : v.id]
-# }
-# # variable for volume wazuh indexer  attachment
+locals {
+  instance_id_ws_set = [for v in aws_instance.bb_server_cluster : v.id]
+}
+# variable for volume wazuh indexer  attachment
 
-# locals {
-#   volume_id_wi_set = [for v in aws_ebs_volume.aws_ebs_volume_wi : v.id]
-# }
+locals {
+  volume_id_wi_set = [for v in aws_ebs_volume.aws_ebs_volume_wi : v.id]
+}
 
-# locals {
-#   instance_id_wi_set = [for v in aws_instance.bb_indexer_cluster : v.id]
-# }
+locals {
+  instance_id_wi_set = [for v in aws_instance.bb_indexer_cluster : v.id]
+}
