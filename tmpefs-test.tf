806800c7c71c57122cde4c316debc0f918d295dc
YDLJ <100246525+lio72@users.noreply.github.com>
2026-02-02 13:06:57 +0100

    fix

--- tmpefs-test.tf
+++ tmpefs-test.tf
@@ -10,14 +10,20 @@ resource "aws_efs_file_system" "efs-app-test" {
 }
 
 # Create Mount Targets
+variable "subnet_ids" {
+  default = [
+    "subnet-0812b5ad4f0ac874a",
+    "subnet-068f84dc34992a380",
+    "subnet-0f6c9b2c10459ec4e"
+  ]
+}
+
 resource "aws_efs_mount_target" "efs-app-test" {
+  count             = length(var.subnet_ids) # Create a mount target for each subnet
   file_system_id    = aws_efs_file_system.efs-app-test.id
-  subnet_id         = ["subnet-0812b5ad4f0ac874a", "subnet-068f84dc34992a380", "subnet-0f6c9b2c10459ec4e"]
+  subnet_id         = var.subnet_ids[count.index]
   security_groups   = ["sg-0daadd71882e1abf4"]
 
   depends_on = [aws_efs_file_system.efs-app-test]
 }
-
-output "efs_id" {
-  value = aws_efs_file_system.efs-app-test.id
 }
