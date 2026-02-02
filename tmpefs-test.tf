f7abadf167552831a0ac05ba860a8926d8780e3c
YDLJ <100246525+lio72@users.noreply.github.com>
2026-02-02 13:02:50 +0100

    add  efs test

--- tmpefs-test.tf
+++ tmpefs-test.tf
@@ -1,24 +1,23 @@
-# # Create EFS
-# resource "aws_efs_file_system" "efs-app-test" {
-#   lifecycle_policy {
-#     transition_to_ia = "AFTER_30_DAYS" # Move files to Infrequent Access after 30 days
-#   }
+# Create EFS
+resource "aws_efs_file_system" "efs-app-test" {
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS" # Move files to Infrequent Access after 30 days
+  }
 
-#   tags = {
-#     Name = "efs-app-test-efs"
-#   }
-# }
+  tags = {
+    Name = "efs-app-test-efs"
+  }
+}
 
-# # Create Mount Targets
-# resource "aws_efs_mount_target" "efs-app-test" {
-#   count             = length(var.subnet_ids)
-#   file_system_id    = aws_efs_file_system.efs-app-test.id
-#   subnet_id         = var.subnet_ids[count.index]
-#   security_groups   = [var.security_group_id]
+# Create Mount Targets
+resource "aws_efs_mount_target" "efs-app-test" {
+  file_system_id    = aws_efs_file_system.efs-app-test.id
+  subnet_id         = var.subnet_ids[subnet-0812b5ad4f0ac874a, subnet-068f84dc34992a380, subnet-0f6c9b2c10459ec4e]
+  security_groups   = [sg-0daadd71882e1abf4]
 
-#   depends_on = [aws_efs_file_system.efs-app-test]
-# }
+  depends_on = [aws_efs_file_system.efs-app-test]
+}
 
-# output "efs_id" {
-#   value = aws_efs_file_system.efs-app-test.id
-# }
+output "efs_id" {
+  value = aws_efs_file_system.efs-app-test.id
+}
