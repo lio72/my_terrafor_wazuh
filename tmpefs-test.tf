46a0f305e5247277a66e15fa18d67250ee327379
YDLJ <100246525+lio72@users.noreply.github.com>
2026-02-02 13:03:57 +0100

    fix

--- tmpefs-test.tf
+++ tmpefs-test.tf
@@ -12,7 +12,7 @@ resource "aws_efs_file_system" "efs-app-test" {
 # Create Mount Targets
 resource "aws_efs_mount_target" "efs-app-test" {
   file_system_id    = aws_efs_file_system.efs-app-test.id
-  subnet_id         = var.subnet_ids[subnet-0812b5ad4f0ac874a, subnet-068f84dc34992a380, subnet-0f6c9b2c10459ec4e]
+  subnet_id         = [subnet-0812b5ad4f0ac874a, subnet-068f84dc34992a380, subnet-0f6c9b2c10459ec4e]
   security_groups   = [sg-0daadd71882e1abf4]
 
   depends_on = [aws_efs_file_system.efs-app-test]
